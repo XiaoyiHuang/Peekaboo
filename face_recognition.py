c3c69edeccae4e6fb4eab8d06888e32610aa7694
XiaoyiHuang <HXY.Marco@gmail.com>
2018-10-21 19:58:42 -0400

    Add face and eye recognition feature

--- face_recognition.py
+++ face_recognition.py
@@ -1,4 +1,5 @@
 from cv2 import *
+import numpy as np
 
 
 def grab_webcam_image():
@@ -10,6 +11,7 @@ def grab_webcam_image():
     s, img = cam.read()
 
     while s:
+        img = face_recognition(img)
         if not show_webcam_img(img):
             break
         s, img = cam.read()
@@ -24,6 +26,31 @@ def show_webcam_img(img):
         return False
     else:
         return True
+
+
+def face_recognition(img):
+    face_cascade = CascadeClassifier('haarcascade_frontalface_default.xml')
+    eye_cascade = CascadeClassifier('haarcascade_eye.xml')
+
+    if face_cascade.empty():
+        raise FileNotFoundError('Failed to locate default face classifier')
+
+    if eye_cascade.empty():
+        raise FileNotFoundError('Failed to locate default eye classifier')
+
+    gray_scale_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray_scale_img, 1.3, 5)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        roi_gray_scale = gray_scale_img[y: y + h, x: x + w]
+        roi_color = img[y: y + h, x: x + w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray_scale)
+        for (ex, ey, ew, eh) in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 255), 2)
+
+    return img
 
 
 def main():
