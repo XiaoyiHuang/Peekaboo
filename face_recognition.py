6b33bef7f0bbf3c80963128c7b4de9252ae20e17
XiaoyiHuang <HXY.Marco@gmail.com>
2018-10-21 17:27:55 -0400

    Add face recognition feature

---
+++ face_recognition.py
@@ -0,0 +1,34 @@
+from cv2 import *
+
+
+def grab_webcam_image():
+    cam = VideoCapture(1)
+
+    if not cam.isOpened():
+        raise IOError("Cannot open webcam")
+
+    s, img = cam.read()
+
+    while s:
+        if not show_webcam_img(img):
+            break
+        s, img = cam.read()
+
+    cam.release()
+
+
+def show_webcam_img(img):
+    imshow("webcam", img)
+    if waitKey(1) == 27:
+        destroyAllWindows()
+        return False
+    else:
+        return True
+
+
+def main():
+    grab_webcam_image()
+
+
+if __name__ == "__main__":
+    main()
